d71cafc0d807b3b5902e2e0cd10d20435afc3b51
learnms802 <learnms802@gmail.com>
2019-09-22 16:45:46 +0300

    new update

--- routes/db.js
+++ routes/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const pgp  =require('pg-promise');
-const conn=pgp('postgres://'+process.env.USERDB+':'+process.env.PASSDB+'@'+process.env.HOSTDB+':'+process.env.PORTDB+'/'+process.env.DBNAME);
-/*const conn = new  pgp.createPool({
+//const conn=pgp('postgres://'+process.env.USERDB+':'+process.env.PASSDB+'@'+process.env.HOSTDB+':'+process.env.PORTDB+'/'+process.env.DBNAME);
+const conn = new  pgp.Pool({
     host: process.env.HOSTDB,
     user: process.env.USERDB,
     password: process.env.PASSDB,
     database: process.env.DBNAME
     });
-*/
+
 module.exports =conn;
